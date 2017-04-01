dcb5798dbb178d33ac7f5805e9aefd1bad165918
Joseph Galindo <joseph.galindo18@gmail.com>
2017-04-01 15:02:16 -0400

    Refactor stuff. Start adding support for help subcommands, based on existing github issue.

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-var fs = require("fs");
+//---EXTERNAL REQUIRES---
+let fs = require("fs");
 const Discord = require("discord.js");
-var memecanvas = require('memecanvas');
-// var ON_DEATH = require('death');
-var credentials = require('./app_credentials.json');
+let memecanvas = require('memecanvas');
+let credentials = require('./app_credentials.json');
+let response_filename = './responses.json';
+let response_data = require(response_filename);
 
+//---INITIAL VARIABLE SETUP---
 const discord_bot = new Discord.Client();
 
-//load response data
-var response_filename = './responses.json';
-var response_data = require(response_filename);
+let mentionResponse = response_data.mentionResponse;
+let multipleTermResponse = response_data.multipleTermResponse;
+let invalidTermResponse = response_data.invalidTermResponse;
+let responses = response_data.responses;
 
-var mentionResponse = response_data.mentionResponse;
-var multipleTermResponse = response_data.multipleTermResponse;
-var invalidTermResponse = response_data.invalidTermResponse;
-var responses = response_data.responses;
-
-var condor_user_id = credentials.bot_user_id;
-var mentionString = "<@" + condor_user_id + ">";
+let condor_user_id = credentials.bot_user_id;
+let mentionString = "<@" + condor_user_id + ">";
 
 //assume by default the message is not from a staff member
-var condor_staff_role = {};
-var isCondorStaff = false;
+let condor_staff_role = {};
+let isCondorStaff = false;
 
 //hardcoding is bad
 const necrodancer_server_id = '83287148966449152';
@@ -28,8 +27,8 @@ const superuser_id = "72432401770352640";
 
 //because this is server-side js, we should just traverse the folder and get the filenames
 //for the moment, I will hardcode these filenames, but should fix this later.
-var image_filenames = ['cadence_dig.jpg', 'cadence_fall.jpg', 'cadence_idk.jpg', 'cadence_resurrect.png', 'cadence_skeleton.jpg', 'cadence_title.jpg', 'necrodancer_dank.jpg'];
-var meme_text_pairs = [
+let image_filenames = ['cadence_dig.jpg', 'cadence_fall.jpg', 'cadence_idk.jpg', 'cadence_resurrect.png', 'cadence_skeleton.jpg', 'cadence_title.jpg', 'necrodancer_dank.jpg'];
+let meme_text_pairs = [
         {'top': 'Some top text',
         'bottom': 'Some bottom text'},
 
@@ -45,29 +44,24 @@ var meme_text_pairs = [
 //initialize memecanvas for .meme command
 memecanvas.init('./images/meme-images', '-meme');
 
-//helper functions
-
-function readyHandler() {
+//---HELPER FUNCTIONS---
+function readyHandler () {
     console.log("Ready, waiting for messages...");
 }
 
-function disconnectHandler() {
+function disconnectHandler () {
     console.log("the bot has disconnected");
-    // discord_bot.removeListener('ready', readyHandler);
 }
 
 //callback function for discord bot login, if needed to debug connection issues.
-function loginFunction(login_results) {
-
-    // console.log(login_results);
+function loginFunction (login_results) {
     console.log("Done attemping login!");
 }
 
 //fires on every message sent
-function checkCondorStaffRole(message_object) {
-
-    var currentGuild = message_object.guild;
-    var authorToGuild, authorRoles;
+function checkCondorStaffRole (message_object) {
+    let currentGuild = message_object.guild;
+    let authorToGuild, authorRoles;
 
     currentGuild.fetchMember(message_object.author)
     .then(function(response) {
@@ -86,23 +80,23 @@ function checkCondorStaffRole(message_object) {
     .catch(console.log);
 };
 
-function generateMemeImage(message_object) {
+function generateMemeImage (message_object) {
     //pick a random image from the array of images, create a valid filepath from that
-    var chosenImage = image_filenames[Math.floor(Math.random()*image_filenames.length)];
+    let chosenImage = image_filenames[Math.floor(Math.random()*image_filenames.length)];
     //pick a random object that has text for the image
-    var chosenText = meme_text_pairs[Math.floor(Math.random()*meme_text_pairs.length)];
+    let chosenText = meme_text_pairs[Math.floor(Math.random()*meme_text_pairs.length)];
 
-    var imagePath = './images/base-images/' + chosenImage;
+    let imagePath = './images/base-images/' + chosenImage;
 
-    var topText = chosenText.top;
-    var bottomText = chosenText.bottom;
+    let topText = chosenText.top;
+    let bottomText = chosenText.bottom;
 
     memecanvas.generate(imagePath, topText, bottomText, function(err, memeFilepath) {
         memeCallback(err, memeFilepath, message_object);
     });
 };
 
-function memeCallback(err, memeFilepath, message_object) {
+function memeCallback (err, memeFilepath, message_object) {
     if(err) {
         console.log(err);
     } else {
@@ -113,7 +107,7 @@ function memeCallback(err, memeFilepath, message_object) {
     }
 };
 
-function deleteMemeImages() {
+function deleteMemeImages () {
     const memeDir = './images/meme-images/';
 
     fs.readdir(memeDir, (err, files) => {
@@ -121,7 +115,7 @@ function deleteMemeImages() {
 
             if(file.indexOf('.png') >= 0) {
                 console.log(file);
-                var real_filepath = memeDir + file; 
+                let real_filepath = memeDir + file;
 
                 fs.unlink(real_filepath, (err) => {
                     if(err) throw err;
@@ -131,20 +125,17 @@ function deleteMemeImages() {
     });
 };
 
-//fires on commands that may be complex (more than one command arg that is not `.infobot`). 
+//fires on commands that may be complex (more than one command arg that is not `.infobot`).
 //`.infobot help` is not complex, but `.infobot edit "tune" "tune text"` is complex.
-function parseCommandsFromMessage(msg_cleaned, message_object) {
-
-    var words = msg_cleaned.split(" ");
-
+function parseCommandsFromMessage (msg_cleaned, message_object) {
+    let words = msg_cleaned.split(" ");
 
     //reassemble the argument that is a string
-    for(var i = 0; i < words.length; i++) {
+    for(let i = 0; i < words.length; i++) {
 
         if(words[i].indexOf("\"") === 0 && words[i].charAt(words[i].length-1) !== "\"") {
-
-            var j = i+1;
-            var quote_string = words[i];
+            let j = i+1;
+            let quote_string = words[i];
 
             while(j < words.length) {
 
@@ -169,16 +160,16 @@ function parseCommandsFromMessage(msg_cleaned, message_object) {
     determineComplexFunction(words, message_object);
 };
 
-//fires on commands that may be complex, after parsing happens in parseCommandsFromMessage. 
-//`.infobot edit "tune" "tune text"` will execute the edit command code here, 
+//fires on commands that may be complex, after parsing happens in parseCommandsFromMessage.
+//`.infobot edit "tune" "tune text"` will execute the edit command code here,
 //but `.infobot madeup_command "123" "text" will do nothing.
 //This function handles that kind of validation and branching.
-function determineComplexFunction(msg_cleaned_split, message_object) {
+function determineComplexFunction (msg_cleaned_split, message_object) {
 
-    if(msg_cleaned_split[0] === "edit") {
-
+    if (msg_cleaned_split[0] === "edit") {
         editCommand(msg_cleaned_split, message_object);
-
+    } else if (msg_cleaned_split[0] === 'help') {
+        helpCommand(msg_cleaned_split, message_object);
     } else {
         message_object.channel.sendMessage(multipleTermResponse)
         .then(/*message => console.log(`Sent message: ${message.content}`)*/)
@@ -189,7 +180,7 @@ function determineComplexFunction(msg_cleaned_split, message_object) {
 //This function fires when determineComplexFunction decides that the command requested is the edit command.
 //Will perform some simple validation to make sure the command to be edited exists.
 //Also checks if the user has CoNDOR Staff Discord role permissions, otherwise it edits nothing.
-function editCommand(msg_cleaned_split, message_object) {
+function editCommand (msg_cleaned_split, message_object) {
 
     //array should look like ['edit', 'command', 'some text here']
     if(msg_cleaned_split.length !== 3) {
@@ -197,8 +188,8 @@ function editCommand(msg_cleaned_split, message_object) {
         .then(/*message => console.log(`Sent message: ${message.content}`)*/)
         .catch(console.log);
     } else {
-        var command_to_edit = msg_cleaned_split[1].replace(new RegExp("\"", 'g'), '');
-        var text_to_edit = msg_cleaned_split[2].replace(new RegExp("\"", 'g'), '');
+        let command_to_edit = msg_cleaned_split[1].replace(new RegExp("\"", 'g'), '');
+        let text_to_edit = msg_cleaned_split[2].replace(new RegExp("\"", 'g'), '');
 
         text_to_edit = "```" + text_to_edit + "```";
 
@@ -211,10 +202,8 @@ function editCommand(msg_cleaned_split, message_object) {
         if(response_data['responses'].hasOwnProperty(command_to_edit)) {
 
             if(isCondorStaff) {
-
                 //make the change to our data within app.js
                 response_data['responses'][command_to_edit] = text_to_edit;
-
                 //then write that data to the actual json file so it is saved for persistence
                 fs.writeFile(response_filename, JSON.stringify(response_data, null, 2), function(err) {
                     // console.log(err);
@@ -235,67 +224,76 @@ function editCommand(msg_cleaned_split, message_object) {
     }
 };
 
-//listeners
+//This function fires each time the .infobot help command is requested.
+//This is set up to help return info about subcommands, if a request like '.infobot help tune' is provided.
+function helpCommand (msg_cleaned_split, message_object) {
+    console.log(msg_cleaned_split.length);
 
+    if (msg_cleaned_split.length !== 1) {
+        // something like .infobot help x
+    } else {
+        // would be like .infobot help
+        message_object.channel.sendMessage(multipleTermResponse)
+        .then(/*message => console.log(`Sent message: ${message.content}`)*/)
+        .catch(console.log);
+    }
+};
+
+//---LISTENER FUNCTIONS---
 discord_bot.on("ready", () => {
     readyHandler();
 });
 
 discord_bot.on("message", (message_object) => {
-
     console.log('Message received at: ' + message_object.createdTimestamp);
 
     //don't let the bot do anything if it's trying to parse its own messages
     if(message_object.author.id !== condor_user_id) {
-
-        var msg_cleaned = message_object.content.trim();
-        var isQuery = msg_cleaned.indexOf(".infobot ") === 0;
-        var isMention = msg_cleaned.indexOf(mentionString) === 0;
+        let msg_cleaned = message_object.content.trim();
+        let isQuery = msg_cleaned.indexOf(".infobot ") === 0;
+        let isMention = msg_cleaned.indexOf(mentionString) === 0;
 
         checkCondorStaffRole(message_object);
 
         if(isQuery) {
-
             msg_cleaned = msg_cleaned.substring(8).trim();
 
             //if space character exists, they used >= 2 keywords.
             //This can mean an invalid query, or it can mean a query with more than 2 keywords.
             //Invalid queries will be redirected, queries will be executed if permissions are met
             if(msg_cleaned.indexOf(" ") >= 0) {
-
                 parseCommandsFromMessage(msg_cleaned, message_object);
-
             } else {
-
                 //a simple command (one term besides `.infobot`) was supplied, such as `.infobot condor`
                 //in this case, just check against the resposnes.json and return text accordingly
-
                 if(responses.hasOwnProperty(msg_cleaned)) {
                     //one keyword, and valid command!!!
                     message_object.channel.sendMessage(responses[msg_cleaned])
                     .then(function(newMsg) {
                         if(msg_cleaned === 'next') {
 
-                            var currentDate = Date.now();
-                            var nextDate = new Date(responses['nextDate']).getTime();
-                            var differenceInSeconds = (nextDate - currentDate)/1000;
+                            let currentDate = Date.now();
+                            let nextDate = new Date(responses['nextDate']).getTime();
+                            let differenceInSeconds = (nextDate - currentDate)/1000;
 
-	 		    console.log(differenceInSeconds);
+                            let days = -1;
+                            let hours = -1;
+                            let minutes = -1;
+                            let seconds = -1;
 
                             if(differenceInSeconds > 0) {
-
-                               var days = Math.floor(differenceInSeconds / (60*60*24));
-                               var hours = Math.floor((differenceInSeconds / (60*60*24) % 1) * 24);
-                               var minutes = Math.floor((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60);
-			       var seconds = Math.floor((((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60) % 1) * 60);
+                               days = Math.floor(differenceInSeconds / (60*60*24));
+                               hours = Math.floor((differenceInSeconds / (60*60*24) % 1) * 24);
+                               minutes = Math.floor((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60);
+			                   seconds = Math.floor((((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60) % 1) * 60);
                             } else {
-                               var days = Math.ceil(differenceInSeconds / (60*60*24));
-                               var hours = Math.ceil((differenceInSeconds / (60*60*24) % 1) * 24);
-                               var minutes = Math.ceil((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60);
-                               var seconds = Math.ceil((((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60) % 1) * 60);
-			    }
-				
-                            var dateMsg = `\`\`\`The next event is ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds away.\`\`\``;
+                               days = Math.ceil(differenceInSeconds / (60*60*24));
+                               hours = Math.ceil((differenceInSeconds / (60*60*24) % 1) * 24);
+                               minutes = Math.ceil((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60);
+                               seconds = Math.ceil((((((differenceInSeconds / (60*60*24) % 1) * 24) % 1) * 60) % 1) * 60);
+			                }
+
+                            let dateMsg = `\`\`\`The next event is ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds away.\`\`\``;
 
                             message_object.channel.sendMessage(dateMsg)
                             .then()
@@ -305,9 +303,7 @@ discord_bot.on("message", (message_object) => {
                     .catch(console.log);
 
                 } else if(msg_cleaned === 'meme' || msg_cleaned === 'memes') {
-
                     generateMemeImage(message_object);
-
                 } else {
                     //one keyword, but not a valid command
                     message_object.channel.sendMessage(invalidTermResponse)
